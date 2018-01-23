58191ce77d85d2b4f0a607fe13a7eff03b7d1705
Christopher Collins <collins.christopher@gmail.com>
2018-01-23 12:03:01 -0500

    initdatabase.py: added check to see if the databse has been populated with users already, and otherwise creates a clean database
    Signed-off-by: Christopher Collins <collins.christopher@gmail.com>

--- initdatabase.py
+++ initdatabase.py
@@ -1,5 +1,13 @@
 from mhn import create_clean_db
-
+from mhn.auth.models import User
+from mhn import mhn
+import sys
 
 if __name__ == '__main__':
-    create_clean_db()
+    with mhn.test_request_context():
+        users = User.query.all()
+        if len(users) >= 1:
+            print("Database already initialized")
+            sys.exit()
+        else:
+            create_clean_db()
