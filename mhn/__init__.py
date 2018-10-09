e1816b60fc8b5995371592702f133158e44c4798
Jesse Bowling <jesse.bowling@duke.edu>
2018-10-09 12:50:18 -0400

    Adjust path

--- mhn/__init__.py
+++ mhn/__init__.py
@@ -137,13 +137,13 @@ def create_clean_db():
         #|-- deploy_snort.sh
         #|-- deploy_kippo.sh
         deployscripts = {
-            'Ubuntu - Conpot': path.abspath('../scripts/deploy_conpot.sh'),
-            'Ubuntu - Dionaea': path.abspath('../scripts/deploy_dionaea.sh'),
-            'Ubuntu - Cowrie': path.abspath('../scripts/deploy_cowrie.sh'),
-            'Ubuntu - Amun': path.abspath('../scripts/deploy_amun.sh'),
-            'Ubuntu - Glastopf': path.abspath('../scripts/deploy_glastopf.sh'),
-            'Ubuntu - Wordpot': path.abspath('../scripts/deploy_wordpot.sh'),
-            'Ubuntu - RDPHoney': path.abspath('../scripts/deploy_rdphoney.sh'),
+            'Ubuntu - Conpot': path.abspath('./scripts/deploy_conpot.sh'),
+            'Ubuntu - Dionaea': path.abspath('./scripts/deploy_dionaea.sh'),
+            'Ubuntu - Cowrie': path.abspath('./scripts/deploy_cowrie.sh'),
+            'Ubuntu - Amun': path.abspath('./scripts/deploy_amun.sh'),
+            'Ubuntu - Glastopf': path.abspath('./scripts/deploy_glastopf.sh'),
+            'Ubuntu - Wordpot': path.abspath('./scripts/deploy_wordpot.sh'),
+            'Ubuntu - RDPHoney': path.abspath('./scripts/deploy_rdphoney.sh'),
         }
         for honeypot, deploypath in deployscripts.iteritems():
             with open(deploypath, 'r') as deployfile:
