2796a50a89b35795cddfa9d197b2b71eb5de7f69
Michael Davis <mdavis332@gmail.com>
2019-04-23 08:26:57 -0500

    Remove hardcoded paths to country flags
    Separate the local path the flag images live from the web path where they live. Allows flexibility in custom route prefixes for the CHN server.

--- mhn/ui/utils.py
+++ mhn/ui/utils.py
@@ -1,5 +1,5 @@
 import requests
-from flask import current_app as app
+from flask import current_app as app, url_for
 from mhn.ui import constants
 from config import MHN_SERVER_HOME
 import os
@@ -10,6 +10,7 @@ from mhn.api.models import Sensor
 
 flag_cache = SimpleCache(threshold=1000, default_timeout=300)
 sensor_cache = SimpleCache(threshold=1000, default_timeout=300)
+
 
 def is_RFC1918_addr(ip):
     # 10.0.0.0 = 167772160
@@ -33,13 +34,14 @@ def is_RFC1918_addr(ip):
 
 def get_flag_ip(ipaddr):
     if is_RFC1918_addr(ipaddr):
-        return constants.DEFAULT_FLAG_URL
+        return url_for('static', filename=constants.DEFAULT_FLAG_URL)
 
     flag = flag_cache.get(ipaddr)
     if not flag:
         flag = _get_flag_ip(ipaddr)
         flag_cache.set(ipaddr, flag)
     return flag
+
 
 def get_sensor_name(sensor_id):
     sensor_name = sensor_cache.get(sensor_id)
@@ -52,12 +54,14 @@ def get_sensor_name(sensor_id):
     print 'Name: %s' % sensor_name
     return sensor_name
 
+
 def _get_flag_ip(ipaddr):
     """
-    Returns an static address where the flag is located.
+    Returns an address where the flag is located.
     Defaults to static immge: '/static/img/unknown.png'
     """
-    flag_path = '/static/img/flags-iso/shiny/64/{}.png'
+    flag_path = url_for(
+        'static', filename='img/flags-iso/shiny/64') + '/{}.png'
     geo_api = 'https://geospray.threatstream.com/ip/{}'
     try:
         # Using threatstream's geospray API to get
@@ -65,12 +69,16 @@ def _get_flag_ip(ipaddr):
         r = requests.get(geo_api.format(ipaddr))
         ccode = r.json()['countryCode']
     except Exception:
-        app.logger.warning("Could not determine flag for ip: {}".format(ipaddr))
-        return constants.DEFAULT_FLAG_URL
+        app.logger.warning(
+            "Could not determine flag for ip: {}".format(ipaddr))
+        return url_for('static', filename=constants.DEFAULT_FLAG_URL)
     else:
         # Constructs the flag source using country code
         flag = flag_path.format(ccode.upper())
-        if os.path.exists(MHN_SERVER_HOME +"/mhn"+flag):
+        local_flag_path = '/static/img/flags-iso/shiny/64/{}.png'.format(
+            ccode.upper())
+
+        if os.path.exists(MHN_SERVER_HOME + "/mhn"+local_flag_path):
             return flag
         else:
-            return constants.DEFAULT_FLAG_URL
+            return url_for('static', filename=constants.DEFAULT_FLAG_URL)
