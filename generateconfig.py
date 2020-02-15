d0c6d399725d0a3fb3c89615a9f63d801dd94d9f
Jesse Bowling <jesse.bowling@duke.edu>
2020-02-14 22:00:44 -0500

    Remove remote IP check code
    Signed-off-by: Jesse Bowling <jesse.bowling@duke.edu>

--- generateconfig.py
+++ generateconfig.py
@@ -4,12 +4,10 @@ working config.py file from the config template.
 """
 
 from getpass import getpass
-import json
 import os.path
 from random import choice
 import string
 import sys
-from urllib.request import urlopen
 
 import argparse
 
@@ -24,8 +22,7 @@ def generate_config():
         print('config.py already exists')
         sys.exit()
 
-    pub_ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
-    default_base_url = 'http://{}'.format(pub_ip)
+    default_base_url = 'https://localhost'
     default_honeymap_url = '{}:3000'.format(default_base_url)
     default_log_path = '/var/log/mhn/mhn.log'
     default_superuser_password = rand_str(32)
